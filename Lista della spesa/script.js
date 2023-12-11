ea88e36271c3e7189adf835b6dc1445540fbff4f
GiorgioDellutri <giorgiodellutridev@gmail.com>
2023-12-11 23:50:52 +0100

    Lista della spesa

---
+++ Lista della spesa/script.js
@@ -0,0 +1,24 @@
+const listaSpesa = [];
+
+while (true) {
+    const vuoiAggiungere = prompt("Vuoi aggiungere un articolo alla lista della spesa? (sì/no)");
+
+    if (vuoiAggiungere.toLowerCase() !== 'si') {
+        break;
+    }
+
+    const nomeArticolo = prompt("Inserisci il nome dell'articolo:");
+    const quantitaArticolo = prompt("Inserisci la quantità dell'articolo:");
+
+    const articolo = {
+        nome: nomeArticolo,
+        quantita: parseInt(quantitaArticolo),
+    };
+
+    listaSpesa.push(articolo);
+}
+
+console.log("Lista della spesa:");
+for (const articolo of listaSpesa) {
+    console.log(`${articolo.nome} - Quantità: ${articolo.quantita}`);
+}
